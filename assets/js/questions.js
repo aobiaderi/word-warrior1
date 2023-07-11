59dcd489d60201e0d053a5a5b680005aaa3005a0
Obee123 <aobiaderi@yahoo.com>
2023-07-11 16:20:10 +0100

    added images to assets folder and updated html file

---
+++ assets/js/questions.js
@@ -0,0 +1,39 @@
+// const questions = [
+//     "What is your favorite color?",
+//     "How many pets do you have?",
+//     "What is your favorite movie genre?",
+//     "Do you prefer coffee or tea?",
+//     "What is your favorite season?",
+// ];
+
+// window.questions = questions;
+
+//This is the Questions array with answers to the quiz 
+let questions = [
+    {
+        title: "What is the fifth planet from the sun?",
+        choices: ["Jupiter", "Mars", "Venus", "Mercury"],
+        answer: "Jupiter"
+    },
+    {
+        title: "Which Galaxy is our solar system in?",
+        choices: ["Cygnus A", "Andromeda", "Milky Way", "Virgo A"],
+        answer: "Milky Way"
+    },
+    {
+        title: " ____ are frozen leftovers from the formation of the solar systems. ",
+        choices: ["Rockets", "Asteriods", "Solar flares", "Comets"],
+        answer: "Comets"
+    },
+    {
+        title: "Where do most comets arrive from?",
+        choices: ["Space debris", "Mars", "Oort Cloud", "None of the above"],
+        answer: "Oort Cloud"
+    },
+    {
+        title: " Which is the largest?",
+        choices: ["Asteriod", "Planet", "Spaceship", "Meteor"],
+        answer: "Planet"
+    },
+
+];
