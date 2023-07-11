2d1e47c58b9dfd58cc75b587bd50a978ba889387
Obee123 <aobiaderi@yahoo.com>
2023-07-11 21:38:12 +0100

    added animation to door

---
+++ assets/js/game.js
@@ -0,0 +1,10 @@
+// Get the door element
+const door = document.getElementById('door');
+
+// Add click event listener to the door
+door.addEventListener('click', function() {
+  // Add 'open' class to the door to trigger the animation
+  door.classList.add('open');
+});
+
+
